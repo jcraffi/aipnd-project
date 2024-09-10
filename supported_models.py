1f75688abd448062162c4351cca59e6f7005dc89
jcraffi <30844161+jcraffi@users.noreply.github.com>
2024-09-10 15:51:50 -0400

    refactor: Add VGG16 model support to supported_models.py

--- supported_models.py
+++ supported_models.py
@@ -1,12 +1,18 @@
 from torch import nn, optim
 from torchvision.models import (
-    VGG13_Weights, DenseNet121_Weights, DenseNet161_Weights, ResNet18_Weights,
+    VGG13_Weights, VGG16_Weights, DenseNet121_Weights, DenseNet161_Weights, ResNet18_Weights,
     ResNet50_Weights, Inception_V3_Weights, EfficientNet_B0_Weights, MobileNet_V2_Weights
 )
 
 model_info = {
     'vgg13': {
         'weights': VGG13_Weights.DEFAULT,
+        'in_features': lambda model: model.classifier[0].in_features,
+        'criterion': nn.NLLLoss,
+        'optimizer': lambda model, lr: optim.Adam(model.classifier.parameters(), lr=lr)
+    },
+    'vgg16': {
+        'weights': VGG16_Weights.DEFAULT,
         'in_features': lambda model: model.classifier[0].in_features,
         'criterion': nn.NLLLoss,
         'optimizer': lambda model, lr: optim.Adam(model.classifier.parameters(), lr=lr)
